9f054f961c8c417c7de37728206d20243b4e36cd
Daniel Sanches <dsanches@ciandt.com>
2020-08-27 02:02:25 -0300

    Incluindo h2 database

---
+++ src/main/resources/data.sql
@@ -0,0 +1,8 @@
+INSERT INTO TB_PESSOA_JURIDICA(CNPJ, NOME_FANTASIA, RAZAO_SOCIAL) VALUES
+('10145422000109', 'The Empire Bakery', 'Padaria do Imperio ME'),('62468145000179', 'Casa de Carnes do Tiao', 'Clodoberto Juarez de Souza');
+
+INSERT INTO TB_PESSOA_FISICA(CPF, NOME) VALUES
+('42878676025', 'Lucas Skywalker'),
+('43101434580', 'Jose da Silva'),
+('47926654345', 'Han Solo'),
+('90677934777', 'Guilhermina Aparecida da Silva');
